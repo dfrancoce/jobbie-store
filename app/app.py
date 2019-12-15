d091da8cb6b5379a7626de679a48987e8b4d77c7
dfrancoce <dfrancoce@protonmail.com>
2019-12-15 23:32:26 +0100

    fix(Dynamodb call): call to the dynamodb module to store the job offers
    Issue: #21

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
+from app.repository import dynamodb
+
+
 def lambda_handler(event, context):
     payloads = []
 
     for record in event['Records']:
         payload = record["body"]
         payloads.append(payload)
+
+    dynamodb.save(payloads)
 
     return {
         "statusCode": 200,
